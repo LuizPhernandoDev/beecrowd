45ca20e45cc410b45d6e1ba3448997587325a841
Luiz Phernando Neves Alves de Souza <luizphernando2905@gmail.com>
2026-07-27 19:28:45 -0300

    exercício 1013

---
+++ 1013.c
@@ -0,0 +1,29 @@
+/*
+Timelimit: 1
+Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da mensagem “eh o maior”. Utilize a fórmula:
+
+maiorAB = (a + b + abs(a-b))/2
+
+Obs.: a fórmula apenas calcula o maior entre os dois primeiros (a e b). Um segundo passo, portanto é necessário para chegar no resultado esperado.
+
+Entrada
+O arquivo de entrada contém três valores inteiros.
+
+Saída
+Imprima o maior dos três valores seguido por um espaço e a mensagem "eh o maior".
+*/
+#include <stdio.h>
+ 
+int main() {
+ 
+    int A, B, C, AB, ABC;
+    
+    scanf("%d %d %d",&A,&B,&C);
+    
+    AB=(A+B+abs(A-B))/2;
+    ABC=(AB+C+abs(AB-C))/2;
+    
+    printf("%d eh o maior\n",ABC);
+    
+    return 0;
+}
