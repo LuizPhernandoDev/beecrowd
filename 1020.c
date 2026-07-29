c1818e59a06409282275d37e560ac1493aaddefd
Luiz Phernando Neves Alves de Souza <luizphernando2905@gmail.com>
2026-07-29 20:00:04 -0300

    exercício 1020

---
+++ 1020.c
@@ -0,0 +1,26 @@
+/*
+Timelimit: 1
+Leia um valor inteiro correspondente à idade de uma pessoa em dias e informe-a em anos, meses e dias
+
+Obs.: apenas para facilitar o cálculo, considere todo ano com 365 dias e todo mês com 30 dias. Nos casos de teste nunca haverá uma situação que permite 12 meses e alguns dias, como 360, 363 ou 364. Este é apenas um exercício com objetivo de testar raciocínio matemático simples.
+
+Entrada
+O arquivo de entrada contém um valor inteiro.
+
+Saída
+Imprima a saída conforme exemplo fornecido.
+*/
+#include <stdio.h>
+ 
+int main() {
+ 
+    int Dias;
+    
+    scanf("%d",&Dias);
+    
+    printf("%d ano(s)\n",Dias/365);
+    printf("%d mes(es)\n",Dias%365/30);
+    printf("%d dia(s)\n",Dias%365%30);
+ 
+    return 0;
+}
