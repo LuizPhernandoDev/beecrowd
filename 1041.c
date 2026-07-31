8d2c642c69a2dfee6b22c9f3d535dddc174cd2f9
Luiz Phernando Neves Alves de Souza <luizphernando2905@gmail.com>
2026-07-30 22:21:24 -0300

    exercício 1044

--- 1041.c
+++ 1041.c
@@ -6,6 +6,12 @@ Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenada
 Se o ponto estiver na origem, escreva a mensagem “Origem”.
 
 Se o ponto estiver sobre um dos eixos escreva “Eixo X” ou “Eixo Y”, conforme for a situação.
+
+Entrada
+A entrada contem as coordenadas de um ponto.
+
+Saída
+A saída deve apresentar o quadrante em que o ponto se encontra.
 */
 #include <stdio.h>
  
