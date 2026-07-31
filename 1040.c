04bb74c5f975d35f9c9a091edcd3fe8c57849c1c
Luiz Phernando Neves Alves de Souza <luizphernando2905@gmail.com>
2026-07-30 22:14:10 -0300

    exercício 1040

---
+++ 1040.c
@@ -0,0 +1,38 @@
+/*
+Timelimit: 1
+Leia quatro números (N1, N2, N3, N4), cada um deles com uma casa decimal, correspondente às quatro notas de um aluno. Calcule a média com pesos 2, 3, 4 e 1, respectivamente, para cada uma destas notas e mostre esta média acompanhada pela mensagem "Media: ". Se esta média for maior ou igual a 7.0, imprima a mensagem "Aluno aprovado.". Se a média calculada for inferior a 5.0, imprima a mensagem "Aluno reprovado.". Se a média calculada for um valor entre 5.0 e 6.9, inclusive estas, o programa deve imprimir a mensagem "Aluno em exame.".
+
+No caso do aluno estar em exame, leia um valor correspondente à nota do exame obtida pelo aluno. Imprima então a mensagem "Nota do exame: " acompanhada pela nota digitada. Recalcule a média (some a pontuação do exame com a média anteriormente calculada e divida por 2). e imprima a mensagem "Aluno aprovado." (caso a média final seja 5.0 ou mais ) ou "Aluno reprovado.", (caso a média tenha ficado 4.9 ou menos). Para estes dois casos (aprovado ou reprovado após ter pego exame) apresente na última linha uma mensagem "Media final: " seguido da média final para esse aluno.
+*/
+#include <stdio.h>
+ 
+int main() {
+ 
+    float N1, N2, N3, N4, Media, Exame;
+    scanf("%f %f %f %f",&N1, &N2, &N3, &N4);
+    N1*=2;
+    N2*=3;
+    N3*=4;
+    Media=(N1+N2+N3+N4)/10;
+    if(Media<7.0&&Media>=5.0){
+        scanf("%f",&Exame);
+    }
+    printf("Media: %.1f\n",Media);
+    if(Media<5.0){
+        printf("Aluno reprovado.\n");
+    }else if(Media<7.0){
+        printf("Aluno em exame.\n");
+        printf("Nota do exame: %.1f\n",Exame);
+        Media=(Media+Exame)/2;
+        if(Media<5.0){
+        printf("Aluno reprovado.\n");
+        }else{
+            printf("Aluno aprovado.\n");
+        }
+        printf("Media final: %.1f\n",Media);
+    }else{
+        printf("Aluno aprovado.\n");
+    }
+    
+    return 0;
+}
