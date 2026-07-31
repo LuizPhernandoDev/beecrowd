22cb7c7a97d74277fcc03acda69b5d148fff4b44
Luiz Phernando Neves Alves de Souza <luizphernando2905@gmail.com>
2026-07-30 22:24:11 -0300

    exercício 1046

---
+++ 1046.c
@@ -0,0 +1,23 @@
+/*
+Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas.
+
+Entrada
+A entrada contém dois valores inteiros representando a hora de início e a hora de fim do jogo.
+
+Saída
+Apresente a duração do jogo conforme exemplo abaixo.
+*/
+#include <stdio.h>
+ 
+int main() {
+ 
+    int Inicio, Fim,Tempo;
+    scanf("%d %d",&Inicio,&Fim);
+    Tempo=Fim-Inicio;
+    if(Inicio>=Fim){
+        Tempo=24+Tempo;
+    }
+    printf("O JOGO DUROU %d HORA(S)\n",Tempo);
+    
+    return 0;
+}
